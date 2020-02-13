a547fb9efb1405a7e1ea43ca04d9c362530122d1
Alexey Repin <repin.alex2099@yandex.ru>
2020-02-13 23:18:41 +0300

    Add two classes

--- Hall of Atonement/Assets/Scripts/NewBehaviourScript.cs
+++ Hall of Atonement/Assets/Scripts/NewBehaviourScript.cs
@@ -16,4 +16,20 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+
+    class Ass
+    {
+        protected void Pokakat()
+        {
+            System.Console.WriteLine("Kakay");
+        }
+    }
+
+    class Hands : Ass
+    {
+        private void ZagrestyGovna()
+        { 
+
+        }
+    }
 }
