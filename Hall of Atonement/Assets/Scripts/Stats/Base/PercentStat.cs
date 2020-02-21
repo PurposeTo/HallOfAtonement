38887379232d913ec1027f18a6813fb1709026b5
PurposeTo <firecrystal1999@gmail.com>
2020-02-21 19:21:31 +0300

    Update PercentStat.cs

--- Hall of Atonement/Assets/Scripts/Stats/Base/PercentStat.cs
+++ Hall of Atonement/Assets/Scripts/Stats/Base/PercentStat.cs
@@ -6,7 +6,7 @@ public class PercentStat : Stat
 {
     //Этот класс всегда в диапазоне от 0 до 100.
     
-    public PercentStat() : base(0f, 0f, 100f) { }
+    public PercentStat() : this(0f) { }
 
     public PercentStat(float baseValue) : base(baseValue, 0f, 100f) { }
 }
