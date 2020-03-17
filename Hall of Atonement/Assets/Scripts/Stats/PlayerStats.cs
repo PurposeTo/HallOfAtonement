3d06b0f47bc00c30448799eb490af55a349ff6b3
PurposeTo <firecrystal1999@gmail.com>
2020-03-17 17:54:06 +0300

    Add Player.Die

--- Hall of Atonement/Assets/Scripts/Stats/PlayerStats.cs
+++ Hall of Atonement/Assets/Scripts/Stats/PlayerStats.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(PlayerController))]
 public class PlayerStats : CharacterStats
@@ -6,5 +7,12 @@ public class PlayerStats : CharacterStats
     public override void GetExperience(int amount, out bool isLvlUp)
     {
         base.GetExperience(amount / 7, out isLvlUp);
+    }
+
+
+    public override void Die(CharacterStats killerStats)
+    {
+        base.Die(killerStats);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //ТУТ! смерть!
     }
 }
