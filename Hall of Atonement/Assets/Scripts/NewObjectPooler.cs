165e0c2f9c368ca1824f80b9ba8b80c89babefc2
PurposeTo <firecrystal1999@gmail.com>
2020-03-19 02:24:58 +0300

    Updating ObjectPooler

---
+++ Hall of Atonement/Assets/Scripts/NewObjectPooler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start()
+    {
+
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        for (int i = 0; i < pools.Count; i++)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+        }
+    }
+}
