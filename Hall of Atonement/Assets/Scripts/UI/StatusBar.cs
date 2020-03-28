f16b85ebbc38a52a2727b8d9c8d8f154d4932d1f
PurposeTo <firecrystal1999@gmail.com>
2020-03-28 23:24:24 +0300

    Starting work on StatusBar

---
+++ Hall of Atonement/Assets/Scripts/UI/StatusBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+    public GameObject ImagePrefab;
+
+    private Transform statusBarContain;
+
+    public List<IStatusEffectLogic> StatusEffects { get; private protected set; } = new List<IStatusEffectLogic>();
+
+
+    private void Start()
+    {
+        statusBarContain = gameObject.transform;
+    }
+
+
+    public void AddStatusEffect(IStatusEffectLogic statusEffect, Sprite sprite = null)
+    {
+        StatusEffects.Add(statusEffect);
+        GameObject statusEffectObject = Instantiate(ImagePrefab, statusBarContain);
+        Image statusEffectImage = statusEffectObject.GetComponent<Image>();
+        statusEffectImage.sprite = sprite;
+
+    }
+
+
+    public void RemoveStatusEffect(IStatusEffectLogic statusEffect)
+    {
+        StatusEffects.Remove(statusEffect);
+    }
+}
